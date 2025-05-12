d8c463e9576ac6878e4a6d2949cdc1acc1f7b41b
chu <87825210+Pengfeicc@users.noreply.github.com>
2025-05-12 14:42:57 +0800

    Update finetune_lora.sh

--- scripts/finetune_lora.sh
+++ scripts/finetune_lora.sh
@@ -3,20 +3,20 @@
 MODEL_NAME="google/gemma-3-4b-it"
 
 # It is strongly recommended to train Gemma3 models with the `eager` attention implementation instead of `flash_attention_2`
-# Cause the GPU limition i used a single A4000 GPU for finetuning, so this script is modified by using A4000 & cpu offload method
+# Cause the GPU limition i used a single A4000 GPU and RTX 3090 for finetuning, so this script is modified by using A4000 & cpu offload method
 export PYTHONPATH=src:$PYTHONPATH
 
 deepspeed src/train/train_sft.py \
     --lora_enable True \
     --vision_lora False \
     --use_dora False \
-    --lora_rank 32 \
+    --lora_rank 32 \     # 64 (if 3090)
     --lora_alpha 128 \
     --lora_dropout 0.05 \
     --lora_namespan_exclude "['lm_head', 'embed_tokens']" \
     --num_lora_modules -1 \
     --use_liger True \
-    --deepspeed scripts/zero3_offload.json \
+    --deepspeed scripts/zero3_offload.json \   # zero3.json (enough GPU memory if 3090)
     --model_id $MODEL_NAME \
     --data_path /path/to/your/training/data.json \
     --image_folder /path/to/your/image/folder \
@@ -28,14 +28,14 @@ deepspeed src/train/train_sft.py \
     --bf16 True \
     --fp16 False \
     --output_dir output/test_lora \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 1 \
-    --learning_rate 1e-4 \
+    --num_train_epochs 4 \
+    --per_device_train_batch_size 1 \    # 2 if 3090 GPU
+    --gradient_accumulation_steps 8 \    # 4 if 3090 GPU
+    --learning_rate 8e-4 \
     --projector_lr 1e-5 \
     --vision_lr 2e-6 \
     --weight_decay 0.1 \
-    --warmup_ratio 0.03 \
+    --warmup_ratio 0.1 \
     --adam_beta2 0.95 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
@@ -44,7 +44,7 @@ deepspeed src/train/train_sft.py \
     --report_to tensorboard \
     --logging_dir output/logs \
     --lazy_preprocess True \
-    --dataloader_num_workers 0 \
+    --dataloader_num_workers 0 \     # 0 if you use a single GPU
     --save_strategy steps \
-    --save_steps 20 \
+    --save_steps 75 \
     --save_total_limit 2 \
